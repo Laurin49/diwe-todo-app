fec9da4c03ff51453cdc9b785e5142e91e0043d4
Laurin49 <welter@diwe.info>
2020-11-15 15:40:46 +0100

    HomeController und Initial Thymleaf Templates ...

---
+++ src/main/java/info/diwe/todoapp/controller/HomeController.java
@@ -0,0 +1,16 @@
+package info.diwe.todoapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public ModelAndView homePage(ModelAndView modelAndView) {
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+
+}
