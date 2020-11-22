1c271c3c5c14cf30e47ff506bc298e629f614a2b
Laurin49 <welter@diwe.info>
2020-11-22 16:22:10 +0100

    Spring Test Deleted to deploy to heroku

--- src/main/java/info/diwe/todoapp/repository/CategoryRepository.java
+++ src/main/java/info/diwe/todoapp/repository/CategoryRepository.java
@@ -2,10 +2,12 @@ package info.diwe.todoapp.repository;
 
 import info.diwe.todoapp.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Calendar;
 import java.util.List;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category findByName(String name);
     Category findFirstBy();
